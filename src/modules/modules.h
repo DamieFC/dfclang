13e21a3920c0f0d25c129434fe87ed87c113a01d
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-06 14:22:05 -0700

    Update modules.h

--- src/modules/modules.h
+++ src/modules/modules.h
@@ -3,4 +3,7 @@
  * (c) 2021 DamieFC                                         *
  ************************************************************/
 
-#include <stdio.h> // For basic text printing
+#include <stdio.h> // For basic text printing & I/O
+#include <math.h> // For math operations
+#include <stdlib.h> // For other things
+#include <ctypes.h> // For text stuff
