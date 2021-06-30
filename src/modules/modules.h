26400f048ee95c07d288059884ff4b3acc7942b1
GNUOverYooouu <dtortoise99@gmail.com>
2021-06-30 14:49:38 -0700

    Fixed some whoopsies

--- src/modules/modules.h
+++ src/modules/modules.h
@@ -3,7 +3,13 @@
  * (c) 2021 DamieFC                                         *
  ************************************************************/
 
+#ifndef _MODULES_H
+#define _MODULES_H
+
 #include <stdio.h> // For basic text printing & I/O
 #include <math.h> // For math operations
 #include <stdlib.h> // For other things
-#include <ctypes.h> // For text stuff
+#include <string.h> // For text manipulation
+#include <ctype.h> // For more text manipulation
+
+#endif // _MODULES_H
