813ebe5086e0b67cbe67c1aaa55fe92b77b3a96c
GNUOverYooouu <dtortoise99@gmail.com>
2021-06-29 19:07:55 -0700

    This project isn't dead

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/**************************************************************
+ * The main file where all exciting stuff the user does       *
+ * happens.                                                   *
+ * (c) 2021 DamieFC under the MIT license                     *
+ **************************************************************/
+
+#include <stdio.h>
+
+#include "modules/modules.h"
+
+int main(){
+  
+}
