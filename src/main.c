cf5a19432696e223d82d7eeb25babfc17774af9e
GNUOverYooouu <dtortoise99@gmail.com>
2021-06-30 14:34:50 -0700

    DUN DUN DUN SOMETHING'SHERENOWYAY!

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 /**************************************************************
  * The main file where all exciting stuff the user does       *
- * happens.                                                   *
+ * happens, AND the compiler is here too!.                    *
  * (c) 2021 DamieFC under the MIT license                     *
  **************************************************************/
 
 #include "modules/modules.h"
 
-int main(){
-  
+void dfclang-compiler(char usage[], char file[]){
+  if(!usage){
+    printf("ERROR: NO USAGE\n");
+    return 1;
+  }
+  else if(strcmp(usage, "help") == 0){
+    printf("The dfclang compiler is used using ./dfclang run <filename>.\n"); // Need to work on
+    printf("All options: \n");
+    printf("\
+           help\n\
+           run\n\
+           "); // lets see if this works :\
+  }
+  /************************************************************
+   * THE COMPILER ITSELF!!!! Sorry nothing really yet :(      *
+   ************************************************************/
+  else if(strcmp(usage[], "run") == 0){
+      
+  }
 }
